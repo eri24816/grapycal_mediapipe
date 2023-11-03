044a15b7d31692e62daa744e50891a8fb59316f1
eri24816 <a931eric@gmail.com>
2023-11-03 15:41:36 +0800

    Update extensions

---
+++ grapycal_torch/activation.py
@@ -0,0 +1,42 @@
+from grapycal import FloatTopic
+from .moduleNode import SimpleModuleNode
+from torch import nn
+
+class ReLUNode(SimpleModuleNode):
+    category = 'torch/nn'
+    inputs = ['inp']
+    max_in_degree = [None]
+    outputs = ['out']
+    display_port_names = False
+
+    def build_node(self):
+        super().build_node()
+        self.label.set('ReLU')
+
+    def create_module(self) -> nn.Module:
+        return nn.ReLU()
+
+    def forward(self, inp):
+        return self.module(inp)
+    
+class LeakyReLUNode(SimpleModuleNode):
+    category = 'torch/nn'
+    inputs = ['inp']
+    max_in_degree = [None]
+    outputs = ['out']
+    display_port_names = False
+
+    def build_node(self):
+        super().build_node()
+        self.label.set('LeakyReLU')
+        self.negative_slope = self.add_attribute('negative_slope', FloatTopic, 0.01, editor_type='float')
+
+    def generate_label(self):
+        return f'LeakyReLU {self.negative_slope.get()}'
+
+    def create_module(self) -> nn.Module:
+        return nn.LeakyReLU(negative_slope=self.negative_slope.get())
+
+    def forward(self, inp):
+        return self.module(inp)
+    
---
+++ grapycal_torch/basic.py
@@ -0,0 +1,33 @@
+from grapycal.extension.utils import NodeInfo
+from .moduleNode import SimpleModuleNode
+from torch import nn
+from grapycal import IntTopic, Node
+
+class LinearNode(SimpleModuleNode):
+    category = 'torch/nn'
+    inputs = ['inp']
+    max_in_degree = [None]
+    outputs = ['out']
+    display_port_names = False
+
+    def build_node(self):
+        super().build_node()
+        self.label.set('Linear')
+        self.in_features = self.add_attribute('in_features',IntTopic,1,editor_type='int')
+        self.out_features = self.add_attribute('out_features',IntTopic,1,editor_type='int')
+
+    def restore_from_version(self, version: str, old: NodeInfo):
+        super().restore_from_version(version, old)
+        self.restore_attributes('in_features','out_features')
+
+    def create_module(self) -> nn.Module:
+        return nn.Linear(self.in_features.get(),self.out_features.get())
+    
+    def generate_label(self):
+        return f'Linear {self.in_features.get()} → {self.out_features.get()}'
+
+    def forward(self, inp):
+        return self.module(inp)
+
+
+    
